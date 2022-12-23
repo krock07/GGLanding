0d766901a646748a686df8dc05864dc1f9f8a430
Khoury Smith <khoury.smith@gmail.com>
2022-12-23 13:40:12 -0500

    updated inidcator buttons

--- components/Main/Carousel.js
+++ components/Main/Carousel.js
@@ -1,15 +1,57 @@
 import React, { useEffect, useState } from "react";
-import { Carousel } from "react-responsive-carousel";
+// import { Carousel } from "react-responsive-carousel";
+import Carousel from "react-responsive-carousel/lib/js/components/Carousel/index";
 import CarouselItems from "./CarouselItems";
+
+const fadeAnimationHandler = (props, state) => {
+  const transitionTime = props.transitionTime + "ms";
+  const transitionTimingFunction = "ease-in-out";
+
+  let slideStyle = {
+    position: "absolute",
+    display: "block",
+    zIndex: 1,
+    minHeight: "100%",
+    opacity: 0,
+    top: 0,
+    right: 0,
+    left: 0,
+    bottom: 0,
+    transitionTimingFunction: transitionTimingFunction,
+    msTransitionTimingFunction: transitionTimingFunction,
+    MozTransitionTimingFunction: transitionTimingFunction,
+    WebkitTransitionTimingFunction: transitionTimingFunction,
+    OTransitionTimingFunction: transitionTimingFunction,
+  };
+
+  if (!state.swiping) {
+    slideStyle = {
+      ...slideStyle,
+      WebkitTransitionDuration: transitionTime,
+      MozTransitionDuration: transitionTime,
+      OTransitionDuration: transitionTime,
+      transitionDuration: transitionTime,
+      msTransitionDuration: transitionTime,
+    };
+  }
+
+  return {
+    slideStyle,
+    selectedStyle: { ...slideStyle, opacity: 1, position: "relative" },
+    prevStyle: { ...slideStyle },
+  };
+};
 
 export default () => (
   <Carousel
     autoPlay
+    swiping={false}
     interval="7000"
     infiniteLoop={true}
     showArrows={false}
     showStatus={false}
     showThumbs={false}
+    // animationHandler={fadeAnimationHandler}
   >
     <CarouselItems
       img="updateVoting.png"
