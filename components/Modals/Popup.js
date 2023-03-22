2cd34a96d3daf854ae6530891a2e093292d93d93
Khoury Smith <khoury.smith@gmail.com>
2023-03-22 17:32:09 -0400

    commitng :Q :Qqquite

--- components/Modals/Popup.js
+++ components/Modals/Popup.js
@@ -12,9 +12,7 @@ export default function Modal({ setOpenModal }) {
           onClick={() => setOpenModal(false)}
         ></div>
         <div className="flex items-center min-h-screen py-8 md:px-4">
-          <div
-            className={`relative w-full md:w-[531px] p-4 mx-auto rounded-[20px] bg-[#2a2a2a] shadow-lg md:h-[531px]`}
-          >
+          <div className="relative w-full md:w-[531px] p-4 mx-auto rounded-[20px] bg-[#2a2a2a] shadow-lg md:h-[531px]">
             <div className="flex text-white rounded-full items-left">
               <AiFillCloseCircle onClick={() => setOpenModal(false)} />
             </div>
